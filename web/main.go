06905041f212bfbd2c0c9885577e421210af10bb
ussii39 <usimarutetunori3@outlook.jp>
2022-04-21 00:10:01 +0900

    first-commit

---
+++ web/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"https://github.com/julienschmidt/httprouter"
+	"github.com/tnkyk/clean_book_go/config"
+	logging "github.com/tnkyk/clean_book_go/handler"
+	"github.com/tnkyk/clean_book_go/handler/rest"
+	"github.com/tnkyk/clean_book_go/infra/persistence"
+	"github.com/tnkyk/clean_book_go/usecase"
+)
+
+func main() {
+	//指定したLogFileにlogを出力するための設定
+	logging.LoggingSetting(config.Config.LogFile)
+
+	userPersistence := persistence.NewUserPersistence()
+	userUseCase := usecase.NewUserUsecase(userPersistence)
+	userHandler := rest.NewUserHandler(userUseCase)
+
+	//httprouterを用いてハンドラーを登録する
+	Router := httprouter.New()
+	Router.GET("/api/users", userHandler.Index)
+	Router.POST("/api/signup", userHandler.SignUp)
+	Router.POST("/api/signin", userHandler.SignIn)
+	//ポート9000で待ち受け
+	err := http.ListenAndServe(":9000", Router)
+	if err != nil {
+		log.Fatalf("Listen and serve failed. %+v", err)
+	}
+	log.Println("Server running...")
+}
